a87fb6f4c7cb870361e7a47b69c90919828fe161
moneymagnate-code <moneymagnate@gmail.com>
2026-07-14 12:27:55 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Bible-WordVerse/',
+  base: '/Bible-WordVerse/', // 👈 critical for GitHub Pages
 })
